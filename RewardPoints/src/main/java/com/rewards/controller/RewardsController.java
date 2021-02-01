219e0b472efbe7c62b9f03d62ef455f042ce4052
adityass3890 <77859299+adityass3890@users.noreply.github.com>
2021-02-02 00:33:03 +0530

    Rewards application changes

---
+++ RewardPoints/src/main/java/com/rewards/controller/RewardsController.java
@@ -0,0 +1,30 @@
+package com.rewards.controller;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rewards.dto.CustomerRewardsDTO;
+import com.rewards.service.RewardsService;
+import com.rewards.util.Util;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class RewardsController {
+	@Autowired
+	private RewardsService rewardsService;
+	
+	@GetMapping("/createData")
+	public String createData() throws ParseException{
+		rewardsService.insertData(Util.getDataSet());
+		return "Sample Data is inserted";
+	}
+	@GetMapping("/getRewardPoints")
+	public CustomerRewardsDTO getRewardPoints(@RequestParam("customerName") String customerName) throws ParseException{
+		return rewardsService.getRewardPoints(customerName);
+	}
+}
